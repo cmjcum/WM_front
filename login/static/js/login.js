29bdffddf98e6f26930beb8b0b3eed600caee463
LULULALA2 <lhg9714@gmail.com>
2022-07-13 19:16:44 +0900

    BUG: modify path

--- login/static/js/login.js
+++ login/static/js/login.js
@@ -1,5 +1,5 @@
 const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5500"
+const frontend_base_url = "https://d26fccab8r7c47.cloudfront.net"
 
 
 async function handleSignup() {
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,7 @@
+const backend_base_url = "http://127.0.0.1:8000"
+const frontend_base_url = "https://d26fccab8r7c47.cloudfront.net"
+
+
 function go_signup() {
     window.location.replace(`${frontend_base_url}/login/signup.html`)
 }
