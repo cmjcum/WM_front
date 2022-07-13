a0781ad7693c07d9806a2155b79a67499fb98c80
LULULALA2 <lhg9714@gmail.com>
2022-07-13 19:22:56 +0900

    BUG: modify path

--- login/static/js/login.js
+++ login/static/js/login.js
@@ -99,3 +99,7 @@ async function handleLogin() {
 function go_signup() {
     window.location.replace(`${frontend_base_url}/login/signup.html`)
 }
+
+function go_login() {
+    window.location.replace(`${frontend_base_url}/login/login.html`)
+}
