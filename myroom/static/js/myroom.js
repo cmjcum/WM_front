d0ed121ec15ea0b3c5fdb6cc97f9f4156086a438
yinmsk <galaxy220111@gmail.com>
2022-07-18 20:07:36 +0900

    ENH 백그라운드 이미지, 메인 크기 조정, 모달 수정

--- myroom/static/js/myroom.js
+++ myroom/static/js/myroom.js
@@ -1,5 +1,4 @@
 function guestbookModal() {
-    console.log("aa");
     const open = () => {
         document.querySelector(".modal").classList.remove("hidden");
     }
@@ -14,34 +13,34 @@ function guestbookModal() {
 }
 
 
-// 회원가입 코드 //////////////////////////////////
-// async function handleSignup() {
-//     const signupData = {
-//         // getElementById 를 사용해 fullname, username, password 데이터를 가져온다.
-//         fullname: document.getElementById('floatingInputFullname').value,
-//         username: document.getElementById("floatingInput").value,
-//         password: document.getElementById('floatingPassword').value,
-//     }
-//     // 가져온 데이터를 fetch를 사용해 해당 url의 headers 에 json 방식으로 넘겨운다.
-//     const response = await fetch(`${backend_base_url}/user/`, {
-//         headers: {
-//             // 어떤 type으로 데이터를 보내줄지 선택한다.
-//             Accept: "application/json",
-//             'Content-type': 'application/json'
-//         },
-//         method: 'POST',
-//         //  body 에는 프론트에서 가져온 signupData 를
-//         // stringify 를 사용해 자바스크립트의 값을 JSON 문자열로 변환한다.
-//         body: JSON.stringify(signupData)
-//     }
-//     )
-//     // response 한 데이터 또한 json 화 해주어야 한다. url에 await 걸어준것 처럼 await 를 걸어준다.
-//     response_json = await response.json()
+async function handleSignup() {
+    const contentData = {
+        content: document.getElementById('contentId').value,
+    }
+    // 가져온 데이터를 fetch를 사용해 해당 url의 headers 에 json 방식으로 넘겨운다.
+    const response = await fetch(`${backend_base_url}/user/<int:owner_id>`, {
+        headers: {
+            // 어떤 type으로 데이터를 보내줄지 선택한다.
+            Accept: "application/json",
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        //  body 에는 프론트에서 가져온 signupData 를
+        // stringify 를 사용해 자바스크립트의 값을 JSON 문자열로 변환한다.
+        body: JSON.stringify(contentData)
+    }
+    )
+    // response 한 데이터 또한 json 화 해주어야 한다. url에 await 걸어준것 처럼 await 를 걸어준다.
+    response_json = await response.json()
 
-//     if (response.status == 200) {
-//         alert('회원 가입 성공')
-//         window.location.reload();
-//     } else {
-//         alert('다시 입력해 주세요')
-//     }
-// }
+    if (response.status == 200) {
+        alert('방명록이 작성됬습니다.')
+        window.location.reload();
+    } else {
+        alert('다시 작성해 주세요')
+    }
+}
+
+
+
+{/* <input type="submit" id="login" class="button margin-top-40" value="Sign In" onclick="handleSignin()"></input> */}
