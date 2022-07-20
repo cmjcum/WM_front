97e64b19c9b3d13eab8bca4af34c46af314af09f
m_ink <mp98dl4@gmail.com>
2022-07-20 17:57:01 +0900

    'board' 브랜치 생성

---
+++ board/static/js/article_api.js
@@ -0,0 +1,15 @@
+// url matching
+const backend_base_url = "http://127.0.0.1:8000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
+// method GET
+// 게시글 상세 페이지
+
+// method POST
+// 게시글 작성하기
+
+// method PUT
+// 게시글 수정하기
+
+// method DELETE
+// 게시글 삭제하기
