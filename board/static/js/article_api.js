726212fdab2932fe97982eb97709e3d91ee6f4cd
m_ink <mp98dl4@gmail.com>
2022-07-25 13:43:29 +0900

    게시판 백-프론트 연결 작업중... 현재 상단바, 메인화면(/board), 게시글 리스트 페이지, 게시글 상세 페이지 대략적으로 구현됨

--- board/static/js/article_api.js
+++ board/static/js/article_api.js
@@ -1,9 +1,51 @@
 // url matching
-const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5500"
+// const backend_base_url = "http://127.0.0.1:8000"
+// const frontend_base_url = "http://127.0.0.1:5500"
 
 // method GET
 // 게시글 상세 페이지
+async function loadArticle() {
+    let board_id = window.location.search.split('board=')[1].split('?')[0]
+    let article_id = window.location.search.split('article=')[1]
+    console.log("load article", board_id, article_id)
+
+    const response = await fetch(`${backend_base_url}/board/${board_id}/${article_id}/`, {
+        method: 'GET',
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("access")
+        },
+        withCredentials: true,
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Object {
+            //      id: 27, 
+            //      planet: 8, 
+            //      author: 3, title: "테스트!!!!!", 
+            //      content: "댓글 달아볼 거예용~~~", 
+            //      picture_url: "", 
+            //      create_date: "22.07.19 04:21:06", 
+            //      comments: [] }
+            console.log(data)
+            author_link_temp = `<a href="/myroom/myroom.html?user=${data.author_id}" class="text-secondary"><i class="bi bi-house-fill"></i></a>`
+            $("#articleTitle").append(data.title)
+            $("#articleAuthor").append(data.author)
+            $("#articleAuthorLink").append(author_link_temp)
+            $("#articleContent").append(data.content)
+            $("#articleDate").append(data.create_date)
+            if (data.picture_url != null) {
+                pic_temp = `<figure>
+                                            <img src="${data.picture_url}">
+                                        </figure>`
+                $("#articlePic").append(pic_temp)
+            }
+
+            // 게시글 작성자인가? > 맞으면 수정/삭제 버튼 출력
+            // 이미지가 있는가? > 없으면 이미지 영역 출력x
+            // 댓글이 있는가? > 없으면 댓글 영역 출력x
+
+        })
+}
 
 // method POST
 // 게시글 작성하기
