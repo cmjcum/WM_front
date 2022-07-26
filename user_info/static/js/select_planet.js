fadef53c8d1c3e7264e5ef0d87610a6c49ae97ed
zeonga1102 <dlwjddk4587@naver.com>
2022-07-26 22:21:23 +0900

    ENH: 서버에서 받아온 행성 정보 planet_info에 저장

--- user_info/static/js/select_planet.js
+++ user_info/static/js/select_planet.js
@@ -1,6 +1,8 @@
 const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
+let planet_info = {}
+
 async function load_planet_info() {
     const response = await fetch(`${backend_base_url}/user/planet/`, {
         method: 'GET',
@@ -9,10 +11,19 @@ async function load_planet_info() {
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            for(let i=0; i<data.length; i++) {
+                cur_data = data[i]
+                planet_info[cur_data['name']] = {
+                    'max_floor': cur_data['max_floor'],
+                    'max_number': cur_data['max_number'],
+                    'population': cur_data['population'],
+                    'empty_rooms': cur_data['empty_rooms']
+                }
+            }
         });
 }
 
 window.onload = function() {
     load_planet_info()
+
 }
