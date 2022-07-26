e2320c90216123892aa088cc0ba869fb315434bb
zeonga1102 <dlwjddk4587@naver.com>
2022-07-27 01:04:43 +0900

    ENH: script_modal 닫는 이벤트 리스너 추가, WIP: select_modal에 데이터들 넣는 함수 작성 중

--- user_info/static/js/select_planet.js
+++ user_info/static/js/select_planet.js
@@ -1,7 +1,7 @@
 const backend_base_url = "http://127.0.0.1:8000"
 const frontend_base_url = "http://127.0.0.1:5500"
 
-let planet_info = {}
+let planet_infos = {}
 
 async function load_planet_info() {
     const response = await fetch(`${backend_base_url}/user/planet/`, {
@@ -13,7 +13,7 @@ async function load_planet_info() {
         .then(data => {
             for(let i=0; i<data.length; i++) {
                 cur_data = data[i]
-                planet_info[cur_data['name']] = {
+                planet_infos[cur_data['name']] = {
                     'max_floor': cur_data['max_floor'],
                     'max_number': cur_data['max_number'],
                     'population': cur_data['population'],
@@ -23,7 +23,25 @@ async function load_planet_info() {
         });
 }
 
+function click_planet(e) {
+    let name = e.target.getAttribute('id')
+    let planet_info = planet_infos[name]
+
+    let floor_select = document.getElementById('floor_select')
+    for(let i=0; i<planet_info['empty_rooms'].length; i++) {
+        
+    }
+    console.log(planet_info['empty_rooms'])
+}
+
 window.onload = function() {
     load_planet_info()
+
+    document.getElementById('click_arrow').addEventListener('click', (e) => { e.target.parentNode.parentNode.style.display = 'none' })
+
+    planets = document.getElementsByClassName('scale')
+    for(let i=0; i<planets.length; i++) {
+        planets[i].addEventListener('click', (e) => {click_planet(e)})
+    }
 
 }
