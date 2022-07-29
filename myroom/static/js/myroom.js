d643c935baadcbed4f46deaca070caa480bf998b
yinmsk <galaxy220111@gmail.com>
2022-07-29 17:51:32 +0900

    ENH 좋아요, 팔로우 클릭시 색상 변경, 팔로우한 사람 리스트 띄우기

--- myroom/static/js/myroom.js
+++ myroom/static/js/myroom.js
@@ -88,14 +88,14 @@ async function show_guest_book() {
                 const author_id = data[i].author_id
                 if (login_user == author_id) {
                     content_temp = `
-                <div class="guestbook">
-                    <button type="button" class="fs-6 guestbook_delete badge rounded-pill bg-primary" onclick="delete_guest(this)"
-                        value=${data[i]["id"]}>삭제</button>
-                    <div class="guestbook_user">
-                        <p class="guestbook_nickname"><b>${nickname}</b><span>&nbsp| ${create_date}</span></p>
-                    </div>
-                    <div class="guestbook_data">${content}</div>
-                </div>`
+                    <div class="guestbook">
+                        <button type="button" class="fs-6 guestbook_delete badge rounded-pill bg-primary" onclick="delete_guest(this)"
+                            value=${data[i]["id"]}>삭제</button>
+                        <div class="guestbook_user">
+                            <p class="guestbook_nickname"><b>${nickname}</b><span>&nbsp| ${create_date}</span></p>
+                        </div>
+                        <div class="guestbook_data">${content}</div>
+                    </div>`
                     $("#guest_book").append(content_temp)
                 } else {
                     content_temp = `
@@ -107,6 +107,7 @@ async function show_guest_book() {
                     </div>`
                     $("#guest_book").append(content_temp)
                 }
+                if (i == 4) break;
             }
         })
 }
@@ -128,7 +129,6 @@ async function show_profile() {
                 const user_id = data[i].user_id
 
                 // UserInfo.data
-                const name = data[i]["name"]
                 const birthday = data[i]["birthday"]
                 const portrait = data[i]["portrait"]
                 const coin = data[i]["coin"]
@@ -136,20 +136,26 @@ async function show_profile() {
                 const room_number = data[i]["room_number"]
                 // User.data
                 const like_count = data[i]["user"]["like_count"]
-                const follow_id = data[i]["user"]["follow"]
                 const follower_count = data[i]["user"]["follower_count"]
                 const follow_count = data[i]["user"]["follow_count"]
-                // planet.data
+                const nickname = data[i]["user"]["nickname"]
+                const like_user = data[i]["user"]["like_user"]
+                const follow_user = data[i]["user"]["follow_user"]
+                
+                // 팔로우한 유저의 정보의 변수
+                const follow_user_data = data[i]["user"]["follow"]
+                
+                // Plant.data
                 const planet = data[i]["planet"]["name"]
 
-
-                for (let i = 0 ; i < follow_id.length ; i++) {
-                    if (follow_id[i] == login_user_id) {
-                        console.log(follow_id[i])
-                    }
-                }
-
-                console.log(follow_id)
+                // // 팔로우한 유저의 정보
+                // for (let i = 0; i < follow_user_data.length; i++) {
+                //     const follow_user_nickname = follow_user_data[i]["follow_user_nickname"]
+                //     const follow_user_portrait = follow_user_data[i]["portrait"]
+                //     console.log(follow_user_nickname)
+                //     console.log(follow_user_portrait)
+                // }
+                // planet.data
 
                 if (login_user_id == user_id) {
                     content_temp = `
@@ -157,7 +163,7 @@ async function show_profile() {
                     <div class="profile-portrait"><img class="profile-portrait" src="${portrait}"></div>
                     <!-- 유저 정보 -->
                     <div class="profile-name" id="profile_name">
-                        <div class="my-profile_name" style="font-size: 12px;">${name}</div>
+                        <div class="my_profile_name" style="font-size: 12px;">${nickname}</div>
                             <div class="card card-body" style="border: 0; padding: 0px; margin: 5px auto auto auto">
                                 <div class="my-profile">행성:&nbsp;${planet}</div>
                                 <div class="my-profile">층수:&nbsp;${room_number} &nbsp; 호수:&nbsp;${floor}</div>
@@ -167,7 +173,7 @@ async function show_profile() {
                     </div>
                 </div>
 
-                <div class="likes_follows-wrap" style="display:flex;">
+                <div class="likes_follows_wrap" style="display:flex;">
                     <div class="profile-name" id="profile_name">
                         <div class="likes_follows">
                             좋아요:${like_count} &nbsp 팔로워:${follower_count} &nbsp 팔로우:${follow_count}
@@ -194,7 +200,7 @@ async function show_profile() {
                     <div class="profile-portrait"><img class="profile-portrait" src="${portrait}"></div>
                     <!-- 유저 정보 -->
                     <div class="profile-name" id="profile_name">
-                        <div class="my-profile_name" style="font-size: 12px;">${name}</div>
+                        <div class="my_profile_name" style="font-size: 12px;">${nickname}</div>
                             <div class="card card-body" style="border: 0; padding: 0px; margin: 5px auto auto auto">
                                 <div class="my-profile">행성:&nbsp;${planet}</div>
                                 <div class="my-profile">층수:&nbsp;${room_number} &nbsp; 호수:&nbsp;${floor}</div>
@@ -202,25 +208,93 @@ async function show_profile() {
                             </div>
                     </div>
                 </div>
-                <div class="likes_follows-wrap" style="display:flex;">
+                <div class="likes_follows_wrap" style="display:flex;">
                     <div class="profile-name" id="profile_name">
                         <div class="likes_follows">
                             좋아요:${like_count} &nbsp 팔로워:${follower_count} &nbsp 팔로우:${follow_count}
                         </div>
                     </div>
                 </div>
-
-                <script>
-                    $(".btn_like").click(function () {
-                        $(this).toggleClass("done");
-                    })
-                </script>
-                <div id="buttons_div" style="float:right;">
-                    <button class="fs-5 btn_like badge rounded-pill" onclick="like_follow()"><i class="bi bi-heart"></i></button>
-                    <button class="fs-4 btn_like badge rounded-pill" onclick="follow_follow()" style="margin-right: 25px;"><i class="bi bi-person-plus"></i></button>
-                </div>
                 `
                     $("#show_profile").append(content_temp)
+                    // 좋아요 팔로우
+                    if (follow_user == true) {
+                        content_temp = `
+                        <script>
+                            $(".btn_like").click(function () {
+                                $(this).toggleClass("done");
+                            })
+                        </script>
+                        <div id="buttons_div" style="float:right;">
+                            <button class="fs-4 btn_like badge rounded-pill" onclick="follow()" style="margin: -1px 15px 0px 0px; color: inherit;"><i class="bi bi-person-plus"></i></button>
+                        </div>
+                        `
+                        $("#show_profile").append(content_temp)
+                    }
+                    else {
+                        content_temp = `
+                        <script>
+                            $(".btn_like").click(function () {
+                                $(this).toggleClass("done");
+                            })
+                        </script>
+                        <div id="buttons_div" style="float:right;">
+                            <button class="fs-4 btn_like badge rounded-pill" onclick="follow()" style="margin: -1px 15px 0px 0px;"><i class="bi bi-person-plus"></i></button>
+                        </div>
+                        `
+                        $("#show_profile").append(content_temp)
+                    }
+                    if (like_user == true) {
+                        content_temp = `
+                    <script>
+                        $(".btn_like").click(function () {
+                            $(this).toggleClass("done");
+                        })
+                    </script>
+                    <div id="buttons_div" style="float:right;">
+                        <button class="fs-5 btn_like badge rounded-pill" onclick="like()" style="color: inherit;"><i class="bi bi-heart"></i></button>
+                    </div>
+                    <div style='height: 50px;'></div>
+                    `
+                        $("#show_profile").append(content_temp)
+                    } else {
+                        content_temp = `
+                    <script>
+                        $(".btn_like").click(function () {
+                            $(this).toggleClass("done");
+                        })
+                    </script>
+                    <div id="buttons_div" style="float:right;">
+                        <button class="fs-5 btn_like badge rounded-pill" onclick="like()"><i class="bi bi-heart"></i></button>
+                    </div>
+                    <div style='height: 50px;'></div>
+                    `
+                        $("#show_profile").append(content_temp)
+                    }
+                    // 팔로우한 유저의 프로필
+                    for (let i = 0; i < follow_user_data.length; i++) {
+                        const follow_user_nickname = follow_user_data[i]["follow_user_nickname"]
+                        const follow_user_portrait = follow_user_data[i]["portrait"]
+                        const follow_user_id = follow_user_data[i]["id"]
+
+                        content_temp = `
+                        <a href="${frontend_base_url}/myroom/myroom.html?user=${follow_user_id}">
+                            <div class="follow_profile_wrap" style="display:flex;">
+                        
+                                <div class="follow_profile_portrait">
+                                    <img class="follow_profile_portrait" src="${follow_user_portrait}">
+                                </div>
+                                
+                                <div class="follow_profile_name">
+                                    <div style="font-size: 13px;">${follow_user_nickname}</div>
+                                </div>
+                            </div>
+                        </a>
+                        `
+                    if (i == 6) break;
+                    $("#show_profile").append(content_temp)
+                    }
+                    // {backend_base_url}/myroom/${login_user_id}/
                 }
             }
         })
@@ -228,7 +302,7 @@ async function show_profile() {
 
 
 // 좋아요
-async function like_follow() {
+async function like() {
     let owner_id = window.location.search.split('=')[1]
     const contentData = {
         content: document.getElementById('guestBookData').value,
@@ -252,6 +326,30 @@ async function like_follow() {
 }
 
 
+async function follow() {
+    let owner_id = window.location.search.split('=')[1]
+    const contentData = {
+        content: document.getElementById('guestBookData').value,
+    }
+
+    const response = await fetch(`${backend_base_url}/myroom/follow/${owner_id}/`, {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access'),
+            Accept: "application/json",
+            'Content-type': 'application/json'
+        },
+        withCredentials: true,
+        method: 'POST',
+
+        body: JSON.stringify(contentData)
+    }
+    )
+    response_json = await response.json()
+    // 새로고침
+    window.location.reload();
+}
+
+// 팔로우 한 사람들의 목록 get / 링크 post
 async function follow_follow() {
     let owner_id = window.location.search.split('=')[1]
     const contentData = {
@@ -272,5 +370,5 @@ async function follow_follow() {
     )
     response_json = await response.json()
     // 새로고침
-    // window.location.reload();
+    window.location.reload();
 }
