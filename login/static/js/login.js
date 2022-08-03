8d803868ec1c3bd7684b8cd88a3bb57930e130d8
LULULALA2 <lhg9714@gmail.com>
2022-08-04 00:05:16 +0900

    url 수정

--- login/static/js/login.js
+++ login/static/js/login.js
@@ -1,5 +1,5 @@
-const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "https://3.38.60.68"
+const frontend_base_url = "https://d26fccab8r7c47.cloudfront.net"
 
 
 async function handleSignup() {
