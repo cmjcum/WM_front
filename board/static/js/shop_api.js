8f5eba671cf49114b2b13193e8752b4946dc67d6
m_ink <mp98dl4@gmail.com>
2022-08-03 11:15:57 +0900

    STY: 상점페이지 구매가능 상품이 0개일 때 출력될 화면 css 조정

--- board/static/js/shop_api.js
+++ board/static/js/shop_api.js
@@ -17,10 +17,9 @@ async function buyFurniture(furniture_id) {
         body: JSON.stringify(contentData)
     }
     )
-    response_json = await response.json()
 
     if (response.status == 200) {
-        window.location.reload();
+        window.location.reload(true);
     } else {
         alert('권한이 없습니다.')
     }
@@ -40,27 +39,40 @@ async function loadFurniture() {
         .then(response => response.json())
         .then(data => {
 
-            for (let i = 0; i < data.length; i++) {
-                let furniture_id = data[i].id
-                let name = data[i].name
-                let img_url = data[i].url_left
-                let price = data[i].price
-                let html_temp = `<div class="col goods mb-3">
-                                                        <div class="card border-primary position-relative" style="max-width: 20rem;">
-                                                            <div class="card-header text-primary">${name}</div>
-                                                            <div class="card-body pt-0">
-                                                                <figure class="d-flex furniture-container">
-                                                                    <img class=" mx-auto"
-                                                                        src="${img_url}">
-                                                                </figure>
-                                                                <h5 class="card-title text-primary">${price} coin</h5>
-                                                                <p class="card-text text-primary mb-3 position-absolute bottom-0 start-50 translate-middle-x"><button type="button"
-                                                                        class="btn btn-secondary" onclick="buyFurniture(${furniture_id})">구입하기</button></p>
-                                                            </div>
+            if (data.length == 0) {
+                let alert_temp = `<div class="card bg-primary px-4 py-4 mt-5 position-absolute top-50 start-50 translate-middle" style="max-width: 400px; height: auto;">
+                                                        <div class="card-body">
+                                                        <h4 class="card-title">새로운 상품이 오는 중이에요<i class="bi bi-truck ms-2" ></i></h4>
+                                                        <p class="card-text">재입고를 기다려주세요!</p>
                                                         </div>
                                                     </div>`
-                $("#goodsList").append(html_temp)
+                $("#alert").append(alert_temp)
+            } else {
+
+                for (let i = 0; i < data.length; i++) {
+                    let furniture_id = data[i].id
+                    let name = data[i].name
+                    let img_url = data[i].url_left
+                    let price = data[i].price
+                    let html_temp = `<div class="col goods mb-3">
+                                                            <div class="card border-primary position-relative" style="max-width: 20rem;">
+                                                                <div class="card-header text-primary">${name}</div>
+                                                                <div class="card-body pt-0">
+                                                                    <figure class="d-flex furniture-container">
+                                                                        <img class=" mx-auto"
+                                                                            src="${img_url}">
+                                                                    </figure>
+                                                                    <h5 class="card-title text-primary">${price} coin</h5>
+                                                                    <p class="card-text text-primary mb-3 position-absolute bottom-0 start-50 translate-middle-x"><button type="button"
+                                                                            class="btn btn-secondary" onclick="buyFurniture(${furniture_id})">구입하기</button></p>
+                                                                </div>
+                                                            </div>
+                                                        </div>`
+                    $("#goodsList").append(html_temp)
+                }
+
             }
+
 
         })
 }
