1b4e8451d6a0d4b85b689d367ff36efa1309893e
m_ink <mp98dl4@gmail.com>
2022-08-05 21:27:31 +0900

    frontend_base_url 수정

--- static/js/url_matching.js
+++ static/js/url_matching.js
@@ -1,7 +1,7 @@
 // url matching
 
 const backend_base_url = "https://coumi.makemigrations.click"
-const frontend_base_url = "https://d26fccab8r7c47.cloudfront.net"
+const frontend_base_url = "https://makemigrations.click"
 
 // const backend_base_url = "http://127.0.0.1:8000"
 // const frontend_base_url = "http://127.0.0.1:5500"
