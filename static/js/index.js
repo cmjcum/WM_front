fa138aaaf15d8d1c35c2cc6d73c9312e4572f1cc
LULULALA2 <lhg9714@gmail.com>
2022-08-12 15:08:34 +0900

    ENH: 로그인시 토큰이 만료가 되지 않았는데도 index.html에서 로그인을 해야하는 문제 해결 #12

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+    skip_login()
+});
+
+
+function skip_login() {
+    let token = localStorage.getItem('access')
+    var base64Url = token.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    let result = JSON.parse(jsonPayload)
+    console.log(result)
+    let token_exp = result['exp']
+    console.log(token_exp)
+    let cur_time = Date.now() / 1000;
+    console.log(cur_time)
+    if (cur_time < token_exp) {
+        window.location.replace(`${frontend_base_url}/board/index.html`)
+    } else {
+        pass
+    }
+}
+
+
+
+
+
+
 function go_signup() {
     window.location.replace(`${frontend_base_url}/login/signup.html`)
 }
